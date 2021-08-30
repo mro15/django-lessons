5352c9a48a4c84dc1f3b489bcaae57b5eb2bd445
Marcela Ribeiro de Oliveira <marcela.ribeiro@alana.ai>
2021-08-29 21:01:29 -0300

    app livro (start)

--- example-project/harveproject/harveproject/settings.py
+++ example-project/harveproject/harveproject/settings.py
@@ -24,7 +24,6 @@ environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY')
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
